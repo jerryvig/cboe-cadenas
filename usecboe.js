c6bd7f3bcafa4f2be20e45c114ca48c084aad7d7
Jerry Vigil <vigilj@google.com>
2014-09-17 16:51:01 -0700

    809

--- usecboe.js
+++ usecboe.js
@@ -5,5 +5,17 @@ var cboe = require('./cboe.js');
 	console.log('QuoteObj = ' + JSON.stringify(quoteObj));
 }); */
 cboe.parseCSVFile(function(rows) {
-	console.log("PARSED ROWS = " + JSON.stringify(rows, undefined, 2));
+	cboe.getRealTimeQuote('aapl', function(quoteObj) {
+		console.log('QuoteObj = ' + JSON.stringify(quoteObj));
+
+		for (var i=0; i<rows.length; i++) {
+			if (new Number(rows[i].strikePrice) >= new Number(quoteObj.quote)) {
+				rows[i].yield = rows[i].bid/quoteObj.quote;
+			} else {
+				rows[i].yield = (rows[i].bid-(quoteObj.quote-rows[i].strikePrice))/quoteObj.quote;
+			}
+		}
+
+		console.log('rows = ' + JSON.stringify(rows, undefined, 2));
+	});
 });
