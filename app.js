879e1e4619f66d65f3ef815b9534a36adda4c81f
Jerry Vigil <vigilj@google.com>
2014-10-02 15:54:36 -0700

    908

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 var express = require('express'),
-	app = express();
+	app = express(),
+	cboe = require('./cboe.js');
 
 app.get('/hello', function(req, res) {
 	res.send('Hello World!');
 });
 
-app.get('/annualizedYields', function(req, res) {
+app.get('/atmView', function(req, res) {
+	console.log("req.query = " + JSON.stringify(req.query, undefined, 2));
 
+	if (req.query.ticker !== undefined) {
+		cboe.getATMView(req.query.ticker, function(atmViewData) {
+			res.send(atmViewData);
+		});
+	} else {
+		res.send('ERROR');	
+	}
 });
-
+	
 app.listen(8081, function(){
 	console.log('Listening on port %d', this.address().port);
 });
